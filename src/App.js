4163ed1d14621266d4e5b0fc8bacd752a0219af9
Mani kandan K <manikandank@shenll.com>
2018-12-08 13:33:38 +0530

    added the widget

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.scss';
 import Header from './components/Header';
 import Banner from './components/Banner';
-
+import Featurewidget from './components/Featurewidget';
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <Banner/>
+        <Featurewidget/>
       </div>
     );
   }
